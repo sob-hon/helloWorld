0d7a9264fe04f20d661aade3be80ff0b1391a1c4
hossein <h.t.a7979@gmail.com>
2021-08-12 09:05:39 +0430

    add slider

--- src/views/HomePage/components/Slider/Slider.jsx
+++ src/views/HomePage/components/Slider/Slider.jsx
@@ -2,7 +2,7 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { CardActionArea, CardContent, CardMedia, makeStyles,Typography,Card, Box } from '@material-ui/core'
 // Import Swiper styles
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css"
@@ -14,30 +14,60 @@ import "./styles.css";
 import SwiperCore, {
   Pagination,Navigation
 } from 'swiper/core';
+import { useMediaQuery } from "@material-ui/core";
 
 // install Swiper modules
 SwiperCore.use([Pagination,Navigation]);
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 export default function Slider() {
   
-  
-  
+  const classes = useStyles();
+  const matchesmd = useMediaQuery('(max-width:900px)');
+  const matchesSm = useMediaQuery('(max-width:600px)');
+
+  const checkSlidesPre = () => {
+    let i = 5
+    if(matchesmd){
+      i = 3
+    }
+    if(matchesSm){
+      i = 1
+    }
+    return i
+  }
   return (
     <>
-    <Swiper slidesPerView={3} spaceBetween={30} slidesPerGroup={3} loop={true} loopFillGroupWithBlank={true} pagination={{
-  "clickable": true
-}} navigation={true} className="mySwiper">
-  <SwiperSlide>Slide 1</SwiperSlide>
-  <SwiperSlide>Slide 2</SwiperSlide>
-  <SwiperSlide>Slide 3</SwiperSlide>
-  <SwiperSlide>Slide 4</SwiperSlide>
-  <SwiperSlide>Slide 5</SwiperSlide>
-  <SwiperSlide>Slide 6</SwiperSlide>
-  <SwiperSlide>Slide 7</SwiperSlide>
-  <SwiperSlide>Slide 8</SwiperSlide>
-  <SwiperSlide>Slide 9</SwiperSlide>
-  </Swiper>
+    <Swiper slidesPerView={checkSlidesPre()} spaceBetween={20} slidesPerGroup={1} loop={true} loopFillGroupWithBlank={true}  navigation={true} className="mySwiper slide">
+
+
+    {
+      [1,2,3,4,5,6,7,8,9,10].map((item) => {
+        return (
+          <SwiperSlide>    
+              <Box style={{height:"250px",width:"200px"}}>
+                <Box display="flex" justifyContent="center" alignItems="center" style={{height:"70%",width:"100%"}}>
+                  <img src="/image/game1.png" style={{height:"100px",width:"100px"}}/>
+                </Box>
+                <Box flexDirection="column" display="flex" alignItems="center" justifyContent="center" style={{height:"30%",width:"100%"}}>
+                  <Typography> نرم افزار </Typography>
+                  <Typography>  {item} </Typography>
+
+                </Box>
+              </Box>
+   
+          </SwiperSlide>
+        )
+      })
+    }
+          
+          
+    </Swiper>
     </>
   )
 }
